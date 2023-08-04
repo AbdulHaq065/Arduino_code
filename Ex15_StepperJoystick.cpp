c438c4256c0d1b9fedd9a8aa11fbb43d24cd3b56
AbdulHaq065 <98849909+AbdulHaq065@users.noreply.github.com>
2023-08-04 14:51:07 +0530

    Add files via upload

---
+++ Ex15_StepperJoystick.cpp
@@ -0,0 +1,50 @@
+#include <AccelStepper.h>
+
+int joyX = A0;
+int joyY = A1;
+
+AccelStepper stepper(8, 10, 12, 11, 13);
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Move Joystick around to change the speed of the stepper motor");
+
+    pinMode(joyX, INPUT);
+    pinMode(joyY, INPUT);
+
+    stepper.setMaxSpeed(1000.0);
+    stepper.setAcceleration(100.0);
+    stepper.setSpeed(200);
+    stepper.moveTo(20000);
+}
+
+void loop()
+{
+    if (analogRead(joyX) < 300 && analogRead(joyY) > 700)
+    {
+        stepper.setSpeed(200);
+    }
+    else if (analogRead(joyX) > 700 && analogRead(joyY) > 700)
+    {
+        stepper.setSpeed(400);
+    }
+    else if (analogRead(joyX) > 700 && analogRead(joyY) < 300)
+    {
+        stepper.setSpeed(600);
+    }
+    else if (analogRead(joyX) < 300 && analogRead(joyY) < 300)
+    {
+        stepper.setSpeed(800);
+    }
+    else
+    {
+        stepper.setSpeed(100);
+    }
+
+    if (stepper.distanceToGo() == 0)
+    {
+        stepper.moveTo(-stepper.currentPosition());
+    }
+    stepper.run();
+}
